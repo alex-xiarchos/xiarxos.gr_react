a7de6fc37800c10420a5c5cc924da6124f65fbc8
Alexandros Xiarchos <36487552+alex-xiarchos@users.noreply.github.com>
2025-05-28 13:56:04 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import Name from "./header/Name.jsx";
-import SocialsBar from "./header/SocialsBar.jsx";
-import HomeButtonsBar from "./header/HomeButtonsBar.jsx";
+import Name from "./Header/Name.jsx";
+import SocialsBar from "./Header/SocialsBar.jsx";
+import HomeButtonsBar from "./Header/HomeButtonsBar.jsx";
 import {HEADER_STYLE, CENTERING_STYLE} from "../TailwindStyles.js";
 import HomeMobile from "./HomeMobile.jsx";
 
